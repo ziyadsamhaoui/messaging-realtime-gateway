876b4294f8271f64854393a8834c887719cf561c
Ziyad SAMHAOUI <ziyad.samhaoui@usms.ac.ma>
2026-09-20 11:49:57 +0000

    initial commit

---
+++ src/main/java/com/ziyadsamhaoui/messagingrealtimegateway/MessagingRealtimeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingrealtimegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRealtimeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingRealtimeGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziyadsamhaoui/messagingrealtimegateway/MessagingRealtimeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingrealtimegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRealtimeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
